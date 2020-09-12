42b6a6188563803ab4fb4e709700eefbc3e4239c
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2020-09-12 13:32:20 +0700

    Render a list

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 import TontonItem from "./components/TontonItem";
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TontonItem title="Chào TonTon" />
-        <TontonItem title="Chào các bạn" />
-        <TontonItem title="Chào các em gái xinh đẹp" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.tontonItems = [
+      "Chào TonTon",
+      "Chào các bạn",
+      "Chào các em gái xinh đẹp",
+    ];
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.tontonItems.map((item, index) => (
+          <TontonItem key={index} title={item} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
