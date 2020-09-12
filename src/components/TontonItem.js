498321fe1a48518ffbb4404c11123bd76de50608
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2020-09-12 13:05:05 +0700

    first commit

---
+++ src/components/TontonItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class TontonItem extends Component {
+  render() {
+    return (
+      <div className="TontonItem">
+        <h2>Hello TonTon</h2>
+      </div>
+    );
+  }
+}
+
+export default TontonItem;
